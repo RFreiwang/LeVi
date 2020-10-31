e6b4997cbdf67e560ec950e19571bb2b1e1dbf55
NeoRuedi <r.freiwang@neobird.de>
2020-10-31 16:29:26 +0100

    small changes

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject Startseite;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,7 @@ public class UIManager : MonoBehaviour
     public void LoadPrefabScrollable(GameObject gameObject)
     {
         Transform Panel = this.transform.GetChild(0);
-        GameObject Artboard = Instantiate(gameObject, Panel.position, Panel.rotation, Panel);
+        GameObject Artboard = Instantiate(gameObject, Panel.position, Panel.rotation, Startseite.transform);
     }
 
     public void LoadPrefab(GameObject gameObject)
